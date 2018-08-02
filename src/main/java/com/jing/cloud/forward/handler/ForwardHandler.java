a93c14b638fd154cbc36d59cbb8f6220aca5f4ff
zhigongzhang <zhigongzhang@zhigongzhang-PC>
2018-08-02 18:07:09 +0800

    首次创建

--- src/main/java/com/jing/cloud/forward/handler/ForwardHandler.java
+++ src/main/java/com/jing/cloud/forward/handler/ForwardHandler.java
@@ -1,14 +1,14 @@
 package com.jing.cloud.forward.handler;
 
-import java.net.Socket;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.jing.cloud.module.Message;
+import com.jing.cloud.module.MessageCode;
+import com.util.RandomUtil;
+
 import io.netty.buffer.ByteBuf;
-import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.channel.group.ChannelGroup;
 
@@ -18,29 +18,36 @@ public class ForwardHandler extends SimpleChannelInboundHandler<ByteBuf> {
 	
 	private ChannelGroup channelGroup;
 	
+	private String token;
+	
 	public ForwardHandler(ChannelGroup channelGroup) {
 		this.channelGroup = channelGroup;
 	}
 
 	@Override
 	public void channelActive(ChannelHandlerContext ctx) throws Exception {
-		logger.info("客户端与服务端连接开始...");
+		logger.info("用户客户端与服务端连接开始...");
+		token = RandomUtil.GetGuid32();
 	}
 
 	@Override
 	public void channelInactive(ChannelHandlerContext ctx) throws Exception {
-		logger.info("客户端与服务端连接关闭...");
+		logger.info("用户客户端与服务端连接关闭...");
 	}
 
 	@Override
 	public void channelRead0(ChannelHandlerContext ctx, ByteBuf msg) throws Exception {
-		
+		byte[] data = new byte[msg.readableBytes()];
+		msg.readBytes(data);
+		Message message = new Message(token, MessageCode.TRANSFER_DATA, data);
+		logger.info("server : " + message);
+		channelGroup.writeAndFlush(message);
 	}
 
 	@Override
 	public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
 		ctx.flush();
-		logger.info("信息接收完毕...");
+		logger.info("接收用户客户端信息完毕...");
 	}
 
 	@Override
